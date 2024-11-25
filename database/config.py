6f7a69b1f2760c1164828dce6343d4bc1907e3fb
Михаил Казьмин <mihail.kazmin.04@mail.ru>
2024-11-25 17:47:33 +0500

    add db connect

---
+++ database/config.py
@@ -0,0 +1,6 @@
+host = "26.170.148.43"#адресс сети или локально "localhost"
+user = "postgres"#ваш пользователь
+password = ""#пароль пользователя
+db_name = "tutordb"#название базы данных
+schema_name = "public"#имя схемы стандартное puplic
+port = 5432
