2e76f772efe3b0252b3f93a274d125ba0a450b2e
FazylovV <vladislav.fazylov04@gmail.com>
2024-11-26 00:49:08 +0500

    fix db

--- database/config.py
+++ database/config.py
@@ -1,6 +1,6 @@
-host = "26.170.148.43"#адресс сети или локально "localhost"
-user = "postgres"#ваш пользователь
-password = ""#пароль пользователя
+host = "localhost"#адресс сети или локально "localhost"
+user = "root"#ваш пользователь
+password = "123"#пароль пользователя
 db_name = "tutordb"#название базы данных
 schema_name = "public"#имя схемы стандартное puplic
 port = 5432
--- database/db.py
+++ database/db.py
@@ -7,54 +7,26 @@ from database.config import host, user, password, db_name, schema_name
 
 class DataBase:
     def __init__(self):
-        self.connection = 0
-
-
-    #Соединение с бд
-    def connect(self):
-        try:
-            self.connection = ps.connect(
+        self.connection = ps.connect(
                 host=host,
                 user=user,
                 password=password,
                 database=db_name
             )
-            self.connection.autocommit = True
-        except Exception as e:
-            print(f"[INFO] Error while connecting to PostgreSQL: {e}")
-
+        self.connection.autocommit = True
 
     # Выполнение SQL-запроса
-    def exec_query(self, update, info_message, is_all_strs=False):
-        try:
-            # Подключаемся к БД
-            connection = ps.connect(
-                host=host,
-                user=user,
-                password=password,
-                database=db_name
-            )
-            connection.autocommit = True
-            with connection.cursor() as cursor:
-                # Выполняем SQL-запрос
-                cursor.execute(update)
-                print(info_message)
-                if is_all_strs:
-                    result = cursor.fetchall()
-                else:
-                    result = cursor.fetchone()
-            return result
-        except Exception as _ex:
-            print("[INFO] Error while working with PostgreSQL", _ex)
-        finally:
-            if connection:
-                connection.close()
-                print("[INFO] PostgreSQL connection closed")
+    def exec_update_query(self, query):
+
+        with self.connection.cursor() as cursor:
+            # Выполняем SQL-запрос
+            cursor.execute(query)
 
 
-    # # Добавление нового пользователя
-    # def add_user(self, id):
-    #     role = 'user'
-    #     self.exec_query(f"""insert into {schema_name}.users (id, role)
-    #                                       values ('{id}', '{role}')""",
-    #                     "[INFO] User was added", True)
+    # Добавление нового пользователя
+    def add_student(self, id, tg_id, fullname, contact):
+        self.exec_update_query(f"""insert into {schema_name}.students (id, tg_id, fullname, contact)
+                                          values ('{id}', '{tg_id}', '{fullname}', '{contact}')""")
+
+db = DataBase()
+db.add_student(2, 2, '2', '2')
